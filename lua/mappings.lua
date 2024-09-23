a79ca3f8088737c5bf3b08117810cfe7cb7ce2c2
efa2d19 <44712637+efa2d19@users.noreply.github.com>
2024-09-23 16:20:04 +0400

    init custom stuff

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,14 @@
-require "nvchad.mappings"
-
--- add yours here
+require("nvchad.mappings")
 
 local map = vim.keymap.set
+local g = vim.g
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+g.VM_default_mappings = 0
+g.VM_maps = {
+    ["Find Under"] = "<C-J>",
+    ["Add Cursor Down"] = "<C-j>",
+    ["Add Cursor Up"] = "<C-k>",
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,35 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        opts = require("configs.conform"),
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+            require("configs.treesitter")
+        end,
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("configs.gitsigns")
+        end,
+    },
+
+    {
+        "mg979/vim-visual-multi",
+        event = { "BufRead", "BufNewFile" },
+    },
 }
