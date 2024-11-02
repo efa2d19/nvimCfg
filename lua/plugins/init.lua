0c9b9f737d8d6b38be890eff98b00e5413639e4d
efa2d19 <44712637+efa2d19@users.noreply.github.com>
2024-11-02 17:29:40 +0400

    fix: added missing plugins for nvui 3.0

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,4 +32,23 @@ return {
         "mg979/vim-visual-multi",
         event = { "BufRead", "BufNewFile" },
     },
+
+    "nvim-lua/plenary.nvim",
+
+    "nvchad/volt",
+
+    {
+        "nvchad/ui",
+        config = function()
+            require "nvchad"
+        end
+    },
+
+    {
+        "nvchad/base46",
+        lazy = true,
+        build = function()
+            require("base46").load_all_highlights()
+        end,
+    },
 }
